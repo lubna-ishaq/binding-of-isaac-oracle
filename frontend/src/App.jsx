75a44dd1bca13c53a4bace59f0e352c019dfff73
Lubna Ishaq <lubna.ishaq@outlook.de>
2026-09-02 08:27:22 +0000

    setting up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div
+      style={{
+        background: "#111",
+        minHeight: "100vh",
+        color: "white",
+        padding: "20px",
+      }}
+    >
+      <h2>ISAAC SYNERGIES GRAPH</h2>
+
+      <p style={{color: "red"}}>
+      all the synergies possible in the binding of isaac are documented here
+      </p>
+    </div>
+  );
+}
+
+export default App;
+``
